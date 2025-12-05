28d592c6d1cb11b1ebbdda7b74be89c002f86c7c
Gilbran Silva de Andrade <122621985+gilbran-ifrn@users.noreply.github.com>
2025-12-05 20:58:14 -0300

    Update extensions.py

--- aplicacao/utils/extensions.py
+++ aplicacao/utils/extensions.py
@@ -26,33 +26,36 @@ REDIRECT_URI = os.getenv("REDIRECT_URI")
 API_BASE_URL = "https://suap.ifrn.edu.br"
 
 def feedback(endpoint, secao, params=None):
-    suap = OAuth2Session(CLIENT_ID, token=current_user.token)
+    try:
+        suap = OAuth2Session(CLIENT_ID, token=current_user.token)
 
-    if params:
-        resposta = suap.get(API_BASE_URL + endpoint, params=params)
-    else:
-        resposta = suap.get(API_BASE_URL + endpoint)
+        if params:
+            resposta = suap.get(API_BASE_URL + endpoint, params=params)
+        else:
+            resposta = suap.get(API_BASE_URL + endpoint)
 
-    #if not resposta.ok:
-    #    return f"Erro {resposta.status_code}. Deu merda"
+        #if not resposta.ok:
+        #    return f"Erro {resposta.status_code}. Deu merda"
 
-    print (resposta.headers)
+        print (resposta.headers)
 
-    print(f"= = = = = = = = = = =")
-    print(f"CÓDIGO: {resposta.status_code}")
-    print(f"= = = = = = = = = = =")
+        print(f"= = = = = = = = = = =")
+        print(f"CÓDIGO: {resposta.status_code}")
+        print(f"= = = = = = = = = = =")
 
-    if resposta.status_code == 200:
-        dados = resposta.json()
-        return render_template('exibeDados.html', dados=dados, secao=secao)
-    elif resposta.status_code == 500:
-        dados = {'mens':'A bronca é do lado do SUAP, não do frontend!'}
-        return render_template('exibeErro.html', erro=resposta.status_code, mens=dados, secao=secao)
-    else:
-        dados = resposta.json()
-        #if resposta:
-        #    dados = resposta.json()
-        #else:
-        #    dados = {'mens':'Não acho que a bronca é minha!'}
-        return render_template('exibeErro.html', erro=resposta.status_code, mens=dados, secao=secao)
+        if resposta.status_code == 200:
+            dados = resposta.json()
+            return render_template('exibeDados.html', dados=dados, secao=secao)
+        elif resposta.status_code == 500:
+            dados = {'mens':'A bronca é do lado do SUAP, não do frontend!'}
+            return render_template('exibeErro.html', erro=resposta.status_code, mens=dados, secao=secao)
+        else:
+            dados = resposta.json()
+            #if resposta:
+            #    dados = resposta.json()
+            #else:
+            #    dados = {'mens':'Não acho que a bronca é minha!'}
+            return render_template('exibeErro.html', erro=resposta.status_code, mens=dados, secao=secao)
+    except Exception as e:
+        return f'Erro: {str(e)}<br><a href="/">Voltar</a>'
     
